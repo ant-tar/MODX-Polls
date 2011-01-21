d058ce64e32820b4e975ea05544f7b5906c93b5c
Bert Oost <bert@oostdesign.nl>
2011-01-21 23:45:28 +0100

    Updated Russian permissions file

--- core/components/polls/lexicon/ru/permissions.inc.php
+++ core/components/polls/lexicon/ru/permissions.inc.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:core/components/polls/lexicon/ru/permissions.inc.php
 ﻿<?php
 /**
  * The permissions Russian Poll lexicon entries
@@ -9,3 +10,7 @@
 $_lang['perm.manage_polls'] = "Управление всеми Опросы записей";
 
 ?>
+=======
+<?php
+$_lang['perm.manage_polls'] = 'Управлять всеми записями опросов';
+>>>>>>> 392b5fadef8fb61f9e8964e3ad26c1058104207a:core/components/polls/lexicon/ru/permissions.inc.php
