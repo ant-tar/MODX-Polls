c932343149e15fd7b07b1fbb36fcf4fe3ee56b10
Bert Oost <bert@oostdesign.nl>
2011-01-22 10:37:15 +0100

    Updated permissions file for German language

--- core/components/polls/lexicon/de/permissions.inc.php
+++ core/components/polls/lexicon/de/permissions.inc.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:core/components/polls/lexicon/de/permissions.inc.php
 ﻿<?php
 /**
  * The permissions German Poll lexicon entries
@@ -8,4 +9,19 @@
 
 $_lang['perm.manage_polls'] = "Verwalten Sie alle Umfragen Einträge";
 
+=======
+<?php
+/**
+ * The permissions lexicon entries
+ *
+ * @package polls
+ * @subpackage lexion
+ * @language: German
+ * @author: Anselm Hannemann (www.anselm-hannemann.de)
+ * @translation-date: 2011-01-22
+*/
+
+$_lang['perm.manage_polls'] = "alle Umfrageneinträge verwalten";
+
+>>>>>>> 9162c8e84b4612839ceca76c5b6fdc710ff9f873:core/components/polls/lexicon/de/permissions.inc.php
 ?>
