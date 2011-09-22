5aa174a552d2c866c65793341892a07a8b62efb0
Wieger Sloot <wieger@sterc.nl>
2011-09-22 14:28:49 +0200

    Added Dutch lexicon
    Signed-off-by: Wieger Sloot <wieger@sterc.nl>

---
+++ core/components/polls/lexicon/nl/latestpoll.inc.php
@@ -0,0 +1,19 @@
+﻿<?php
+/**
+ * The latest poll English Poll lexicon entries
+ *
+ * @package polls
+ * @subpackage lexion
+ * @language: English
+ * @author: Bert Oost
+ * @translation-date: 2010-12-21
+ */
+
+$_lang['polls.totalvotes'] = "Totaal aantal stemmen: [[+num]]";
+$_lang['polls.vote'] = "Stem!";
+$_lang['polls.votedon'] = "Je hebt al gestemd [[+date]] op [[+time]]!";
+$_lang['polls.category'] = "Categorie: [[+name]]";
+$_lang['polls.votes'] = "Stemmen: [[+num]]";
+$_lang['polls.showresults'] = "Bekijk resultaten";
+
+?>
