9f5c1cced3d117eb85fd889ee8f5948f4066a472
Bert Oost <bert@server.oostdesign.nl>
2011-11-11 20:52:47 +0100

    Added Dutch lexicons

---
+++ core/components/polls/lexicon/nl/default.inc.php
@@ -0,0 +1,74 @@
+﻿<?php
+/**
+ * The default English Poll lexicon entries
+ *
+ * @package polls
+ * @subpackage lexion
+ * @language: English
+ * @author: Bert Oost
+ * @translation-date: 2011-01-20
+ */
+
+$_lang['polls'] = "Polls";
+$_lang['polls.manage'] = "Beheer Polls";
+$_lang['polls.desc'] = "Beheer hier poll categorieën, vragen en antwoorden.";
+$_lang['polls.search'] = "Zoek...";
+$_lang['polls.publishdate'] = "Datum Publicatie";
+$_lang['polls.unpublishdate'] = "Datum Depublicatie";
+$_lang['polls.hide'] = "Verberg";
+$_lang['polls.duplicate'] = "Dupliceer";
+
+// error strings
+$_lang['polls.error.griddata'] = "Geen gegevens van dit grid gevonden!";
+$_lang['polls.error.update'] = "Kon record niet updaten, omdat record onbekend is!";
+$_lang['polls.error.save'] = "Opslaan van gegevens is mislukt!";
+
+// questions part
+$_lang['polls.questions'] = "Vragen";
+$_lang['polls.question'] = "Vraag";
+$_lang['polls.questionid'] = "VID";
+$_lang['polls.question.nocategory'] = "Geen categorie";
+$_lang['polls.questions.desc'] = "Beheer hier uw vragen die u wilt tonen op de website. U kunt ook poll verbergen van de site of ze publiceren of depubliceren door data in te stellen. Wijzig de waarden eenvoudig door erop te klikken.";
+$_lang['polls.questions.create'] = "Maak vraag";
+$_lang['polls.questions.create.error_save'] = "Opslaan van de nieuwe vraag is mislukt. Probeer het opnieuw!";
+$_lang['polls.questions.votes'] = "Stemmen";
+$_lang['polls.questions.duplicate'] = "Dupliceer vraag en antwoorden";
+$_lang['polls.questions.duplicate_confirm'] = "Weet u zeker dat u de vraag en anwoorden wilt dupliceren? Resultaten (zoals het aantal stemmen) worden niet gedupliceerd.";
+$_lang['polls.questions.duplicate_error'] = "Dupliceren van vraag en/of antwoorden is mislukt. Probeer het opnieuw!";
+$_lang['polls.questions.update'] = "Bewerk vraag";
+$_lang['polls.questions.remove'] = "Verwijder vraag";
+$_lang['polls.questions.remove_confirm'] = "Weet u zeker dat u deze vraag en de bijhorende antwoorden en gegevens wilt verwijderen?";
+$_lang['polls.questions.error_remove'] = "Verwijderen van de vraag is mislukt. Probeer het opnieuw!";
+
+$_lang['polls.question.update'] = "Bewerk vraag";
+$_lang['polls.question.error_update'] = "Bewerken vraag is mislukt. Probeer het opnieuw!";
+$_lang['polls.questions.errorload'] = "Kan de vraag niet tonen!";
+
+// categories part
+$_lang['polls.categories'] = "Categorie&euml;n";
+$_lang['polls.category'] = "Categorie";
+$_lang['polls.category.filter'] = "Filter op Categorie";
+$_lang['polls.categoryid'] = "Categorie ID";
+$_lang['polls.categories.desc'] = "Hieronder staat een lijst met categorie&euml;n voor de vragen. U kunt een categorie bewerken door erop te klikken. U kunt een categorie verwijderen via een klik met de rechter muisknop.";
+$_lang['polls.categories.create'] = "Maak Categorie";
+$_lang['polls.categories.create.error_save'] = "Opslaan nieuwe categorie mislukt. Probeer het opnieuw!";
+$_lang['polls.categories.remove'] = "Verwijder Categorie";
+$_lang['polls.categories.remove_confirm'] = "Weet u zeker dat u deze categorie en de bijhorende vragen wilt verwijderen?";
+$_lang['polls.categories.error_remove'] = "Verwijderen van de categorie is mislukt. Probeer het opnieuw!";
+
+// answers part
+$_lang['polls.answers'] = "Antwoorden";
+$_lang['polls.answer'] = "Antwoord";
+$_lang['polls.answerid'] = "Antwoord ID";
+$_lang['polls.answers.btnback'] = "Terug naar vragen";
+$_lang['polls.answers.desc'] = "Beheer hier uw antwoorden op de geselecteerde vraag. U kunt een antwoord bewerken door erop te klikken, en u kunt het sorteernummer wijzigen om de positie te veranderen. Let op: een antwoord met sorteernummer 0 is altijd het laatst in de lijst.";
+$_lang['polls.answers.votes'] = "Stemmen";
+$_lang['polls.answers.percents'] = "Procent";
+$_lang['polls.answers.sort'] = "Sortering";
+$_lang['polls.answers.create'] = "Maak Antwoord";
+$_lang['polls.answers.create.error_save'] = "Maken nieuw antwoord mislukt. Probeer het opnieuw!";
+$_lang['polls.answers.remove'] = "Verwijder antwoord!";
+$_lang['polls.answers.remove_confirm'] = "Weet u zeker dat u dit antwoord wilt verwijderen?";
+$_lang['polls.answers.error_remove'] = "Verwijderen van het antwoord is mislukt. Probeer het opnieuw!";
+
+?>
---
+++ core/components/polls/lexicon/nl/latestpoll.inc.php
@@ -0,0 +1,19 @@
+﻿<?php
+/**
+ * The latest poll English Poll lexicon entries
+ *
+ * @package polls
+ * @subpackage lexion
+ * @language: English
+ * @author: Bert Oost
+ * @translation-date: 2010-12-21
+ */
+
+$_lang['polls.totalvotes'] = "Totaal aantal stemmen: [[+num]]";
+$_lang['polls.vote'] = "stem!";
+$_lang['polls.votedon'] = "U hebt al gestemd op [[+date]] om [[+time]]!";
+$_lang['polls.category'] = "categorie: [[+name]]";
+$_lang['polls.votes'] = "stemmen: [[+num]]";
+$_lang['polls.showresults'] = "Toon resultaten";
+
+?>
