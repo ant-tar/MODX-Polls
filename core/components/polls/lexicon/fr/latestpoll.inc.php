9b6cda9ad2d3a15e1e0dbb152ea77f81f27b969d
Romain Fallet <romain.fallet@outlook.com>
2014-07-16 17:55:32 +0200

    Add fr lexicon

---
+++ core/components/polls/lexicon/fr/latestpoll.inc.php
@@ -0,0 +1,19 @@
+﻿<?php
+/**
+ * The latest poll French Poll lexicon entries
+ *
+ * @package polls
+ * @subpackage lexion
+ * @language: Français
+ * @author: Romain Fallet
+ * @translation-date: 2014-07-16
+ */
+
+$_lang['polls.totalvotes'] = "Total des votes : [[+num]]";
+$_lang['polls.vote'] = "Voter";
+$_lang['polls.votedon'] = "Vous avez déjà voté le [[+date]] à [[+time]]!";
+$_lang['polls.category'] = "Categorie : [[+name]]";
+$_lang['polls.votes'] = "Votes : [[+num]]";
+$_lang['polls.showresults'] = "Montrer les résulats";
+
+?>
