acf62d7615eb18324a905c608033ef8f57891e0a
Romain Fallet <romain.fallet@outlook.com>
2014-07-17 11:15:39 +0200

    Fr lexicon corrections
    Corrections of some translation mistakes.

--- core/components/polls/lexicon/fr/latestpoll.inc.php
+++ core/components/polls/lexicon/fr/latestpoll.inc.php
@@ -11,9 +11,9 @@
 
 $_lang['polls.totalvotes'] = "Total des votes : [[+num]]";
 $_lang['polls.vote'] = "Voter";
-$_lang['polls.votedon'] = "Vous avez déjà voté le [[+date]] à [[+time]]!";
+$_lang['polls.votedon'] = "Vous avez déjà voté le [[+date]] à [[+time]].";
 $_lang['polls.category'] = "Categorie : [[+name]]";
 $_lang['polls.votes'] = "Votes : [[+num]]";
-$_lang['polls.showresults'] = "Montrer les résulats";
+$_lang['polls.showresults'] = "Montrer les résultats";
 
 ?>
