548d9b288bf198273d22c155081fcb4a1452c038
matdave <mat@matdave.com>
2021-03-18 11:46:04 -0500

    sanitize get request

--- core/components/polls/controllers/mgr/polls_header.php
+++ core/components/polls/controllers/mgr/polls_header.php
@@ -5,13 +5,17 @@
  * @package polls
  * @subpackage controllers
  */
+$modxtags = array_values($modx->sanitizePatterns);
+$depth = (int)ini_get('max_input_nesting_level');
+$depth = ($depth <= 0) ? 99 : $depth + 1;
+$get =  $modx->sanitize($_GET, $modxtags, $depth);
 
 $modx->regClientStartupScript($polls->config['jsUrl'].'mgr/polls.js');
 $modx->regClientStartupHTMLBlock('<script type="text/javascript">
 Ext.onReady(function() {
     Polls.config = '.$modx->toJSON($polls->config).';
     Polls.config.connector_url = "'.$polls->config['connectorUrl'].'";
-    Polls.request = '.$modx->toJSON($_GET).';
+    Polls.request = '.json_encode($get).';
 });
 </script>');
 
